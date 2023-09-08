3c060403575e56e624a8138237791bdee21353a9
Tian <ttan14@jh.edu>
2023-09-08 13:26:14 -0400

    day4-WrightFisher Ex2 pt2

--- day4-homework/WrightFisher.py
+++ day4-homework/WrightFisher.py
@@ -43,20 +43,30 @@ def wright_fisher(start_AlFreq, pop_size):
 
 fig,ax=plt.subplots()
 
-for i in range(30):
-	traject=[]
-	x_traject=wright_fisher(0.5,100)
-	y_traject=[]
-	n=1
-	for j in range(len(x_traject)):
-		n+=1
-		y_traject.append(n)
+#Ex2
+# for i in range(30):
+# 	x_traject=[]
+# 	x_traject=wright_fisher(0.5,100)
+# 	y_traject=[]
+# 	n=1
+# 	for j in range(len(x_traject)):
+# 		n+=1
+# 		y_traject.append(n)
 
-	ax.plot(y_traject,x_traject)
+# 	ax.hist(y_traject,x_traject)
 
-ax.set_xlabel("Generations")
-ax.set_ylabel("Allele frequency")
-fig.savefig("WrightFisher-Ex2.png")
+#Ex2 
+x_traject=[]
+for i in range(1000):
+ 	y_traject=wright_fisher(0.5,100)
+ 	for j in y_traject:
+ 		x_traject.append(len(y_traject))	
+plt.hist(x_traject)
+
+
+ax.set_xlabel("Times to fixation")
+ax.set_ylabel("Number of occurrences")
+fig.savefig("WrightFisher-Ex2 pt2.png")
 
 plt.show()
 
