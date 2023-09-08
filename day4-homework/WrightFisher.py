da352d0e9d404f812bb71022e94998698f31d72c
Tian <ttan14@jh.edu>
2023-09-08 14:10:01 -0400

    day4-WrightFisher Ex3 pt1

--- day4-homework/WrightFisher.py
+++ day4-homework/WrightFisher.py
@@ -43,7 +43,7 @@ def wright_fisher(start_AlFreq, pop_size):
 
 fig,ax=plt.subplots()
 
-#Ex2
+#Ex2 pt1
 # for i in range(30):
 # 	x_traject=[]
 # 	x_traject=wright_fisher(0.5,100)
@@ -53,22 +53,81 @@ fig,ax=plt.subplots()
 # 		n+=1
 # 		y_traject.append(n)
 
-# 	ax.hist(y_traject,x_traject)
+# 	ax.plot(y_traject,x_traject)
+#ax.set_xlabel("Generations")
+#ax.set_ylabel("Allele frequency")
+#fig.savefig("WrightFisher-Ex2.png")
 
-#Ex2 
-x_traject=[]
-for i in range(1000):
- 	y_traject=wright_fisher(0.5,100)
- 	for j in y_traject:
- 		x_traject.append(len(y_traject))	
-plt.hist(x_traject)
+#plt.show()
+
+##############################################################
+
+#Ex2 pt2 histogram
+# x_traject=[]
+# for i in range(1000):
+#  	y_traject=wright_fisher(0.5,100)
+#  	for j in y_traject:
+#  		x_traject.append(len(y_traject))	
+# plt.hist(x_traject)
 
 
-ax.set_xlabel("Times to fixation")
-ax.set_ylabel("Number of occurrences")
-fig.savefig("WrightFisher-Ex2 pt2.png")
+# ax.set_xlabel("Times to fixation")
+# ax.set_ylabel("Number of occurrences")
+# fig.savefig("WrightFisher-Ex2pt2.png")
 
+# plt.show()
+
+
+##############################################################
+#Ex3 pt1
+
+pop_input=[60,70,80,90,100]
+num_fix=[]
+aver_fix=0
+aver_plot=[]
+for i in range(5):
+	al_freq=[]
+	num_fix=[]
+	sum=0
+	for j in range(60):
+		al_freq.append(wright_fisher(0.5,pop_input[i]))
+		num_fix.append(len(al_freq[0])) #num fix one time
+		al_freq=[]
+
+	num_fix=np.array(num_fix) #num to fix for 60 times
+	aver_fix=np.mean(num_fix) #average of the 60 times for one pop size
+	aver_plot.append(aver_fix) #list of 5 pop size with average num to fix 
+
+
+ax.scatter(pop_input,aver_plot)
+ax.set_xlabel("Population sizes")
+ax.set_ylabel("Average number to fixations (60 trials)")
+fig.savefig("WrightFisher-Ex3pt1.png")
 plt.show()
+
+###########################################################
+# #Ex3 pt2
+
+# salf_input=[0.1,0.2,0.3,0.4,0.5]
+# num_fix=[]
+# aver_fix=0
+# aver_plot=[]
+# for i in range(5):
+# 	al_freq=[]
+# 	num_fix=[]
+# 	sum=0
+# 	for j in range(10):
+# 		al_freq.append(wright_fisher(salf_input[i],50))
+# 		num_fix.append(len(al_freq[0])) #num fix one time
+# 		al_freq=[]
+
+# 	num_fix=np.array(num_fix) #num to fix for 60 times
+# 	aver_fix=np.mean(num_fix) #average of the 60 times for one pop size
+# 	aver_plot.append(aver_fix) #list of 5 pop size with average num to fix 
+
+# print(aver_plot)
+
+
 
 
 
