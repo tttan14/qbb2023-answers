2f08ffc0987484477c3c5a924bf4384aa518c66c
Tian <ttan14@jh.edu>
2023-09-21 19:11:19 -0400

    Exercise 2

---
+++ week1/README.md
@@ -0,0 +1,19 @@
+2.2
+1) The slope of maternal age in response to maternal de novo muation is 0.6026 and 19.5164 y-intercept. This means the maternal age is changing at a rate of 0.60226 in response to the maternal de novo mutation variable. It does match because from the scatterplot, maternal age does seem to increase positively as there are more maternal DNMs.
+2) It is significant because the p value is  6.878208052157195e-24 and is smaller than 0.05
+
+2.3
+1) The slope of paternal age in response to paternal de novo muation is 0.4571 and 7.0178 y-intercept. This means the paternal age is changing at a rate of 0.4571 in response to the paternal de novo mutation variable. It matches because from the scatterplot, it seems like there is a positive increase of paternal DNMs as paternal age increases.
+2)  It is significant because the p value is 1.5522936156889328e-84 and is smaller than 0.05
+
+2.4
+y=0.4571x+7.0178 (y: paternal age; x: paternal age)
+At 50.5 years old:
+(50.5)=0.4571x+7.0178
+x=95.13
+The number of paternal DNMs is about 95.
+
+2.6
+I chose two-sample t test because there are two independent samples (maternal and paternal DNMs) drawn from a population with unknown mean.
+The test statistics were signicant. The p value is 2.1986031793078793e-264 which is smaller than 0.05. The test is suggesting that the difference between is very different and mean of paternal DNMs is much larger than the average of maternal DNMs. In generally, there are likely more paternal inherited DNMs.
+
--- week1/week1.py
+++ week1/week1.py
@@ -3,6 +3,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import statsmodels.formula.api as smf
+import scipy.stats as sps
+
 
 #EXERCISE 1
 #########################################################################
@@ -40,6 +43,71 @@ dwage=pd.read_csv('aau1043_parental_age.csv',sep=',',header=0,index_col=0)
 #1.5 
 
 merged=pd.concat([deNovoCountDF,dwage],axis=1,join='inner')
+
+#EXERCISE 2
+#########################################################################
+#2.1
+
+fig, ax = plt.subplots()
+ax.scatter(merged.loc[:,'maternal_dnm'],merged.loc[:,'Mother_age'])
+ax.set_xlabel("maternal de novo mutations")
+ax.set_ylabel("maternal age")
+ax.set_title( "maternal de novo mutations vs. maternal age" )
+#fig.savefig( "ex2_a.png" )
+#plt.show()
+
+fig1, ax1 = plt.subplots()
+ax1.scatter(merged.loc[:,'paternal_dnm'],merged.loc[:,'Father_age'])
+ax1.set_xlabel("paternal de novo mutations")
+ax1.set_ylabel("paternal age")
+ax1.set_title( "paternal de novo mutations vs. paternal age" )
+#fig1.savefig( "ex2_b.png" )
+#plt.show()
+
+#########################################################################
+#2.2
+
+merged_1=pd.concat([merged.loc[:,'maternal_dnm'],merged.loc[:,'Mother_age']],axis=1,join='inner')
+model1 = smf.ols(formula='Mother_age ~1 + maternal_dnm',data=merged_1)
+results1 = model1.fit()
+print(results1.summary())
+print('maternal pvalues: ',results1.pvalues['maternal_dnm'])
+
+#########################################################################
+#2.3
+
+merged_2=pd.concat([merged.loc[:,'paternal_dnm'],merged.loc[:,'Father_age']],axis=1,join='inner')
+model2 = smf.ols(formula='Father_age ~1 + paternal_dnm',data=merged_2)
+results2 = model2.fit()
+print(results2.summary())
+print('paternal pvalues: ',results2.pvalues['paternal_dnm'])
+#########################################################################
+#2.5
+
+fig2, ax2 = plt.subplots()
+#merged.loc[:,'paternal_dnm'].plot.bar()
+ax2.hist(merged.loc[:,'paternal_dnm'],label='paternal de novo mutations',alpha=0.5)
+ax2.hist(merged.loc[:,'maternal_dnm'],label='maternal de novo mutations',alpha=0.5)
+ax2.legend()
+ax2.set_xlabel("de novo mutation counts")
+ax2.set_ylabel("Occurrences")
+#fig2.savefig( "ex2_c.png" )
+plt.show()
+
+#########################################################################
+#2.6
+
+mean1=merged_1.loc[:,'maternal_dnm'].mean()
+mean2=merged_2.loc[:,'paternal_dnm'].mean()
+
+std1=merged_1.loc[:,'maternal_dnm'].std()
+std2=merged_2.loc[:,'paternal_dnm'].std()
+
+print(sps.ttest_ind_from_stats(mean1, std1, len(merged_1.loc[:,'maternal_dnm']), mean2, std2, len(merged_2.loc[:,'paternal_dnm']), equal_var=True, alternative='two-sided'))
+
+
+
+
 
 
 
