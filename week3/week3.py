b6607f88c50e69dec920fd2fde1ecff0b0ea3cf4
Tian <ttan14@jh.edu>
2023-11-22 20:06:01 -0500

    week3 re

--- week3/week3.py
+++ week3/week3.py
@@ -76,11 +76,10 @@ seq2_id, sequence2 = input_sequences[1][0],input_sequences[1][1]
 F_matrix=np.zeros((len(sequence1)+1,len(sequence2)+1))
 TB_matrix=np.zeros((len(sequence1)+1,len(sequence2)+1),str)
 
-for i in range(len(sequence1)+1):
-	F_matrix[i,0]=i*gap_penalty
-
-for j in range(len(sequence2)+1):
-	F_matrix[0,j]=j*gap_penalty
+def initialize_matrices(m, n):
+    F_matrix = [[0] * (n + 1) for _ in range(m + 1)]
+    TB_matrix = [[''] * (n + 1) for _ in range(m + 1)]  # Renamed the traceback matrix to TB
+    return F_matrix, TB_matrix
 
 for i in range(1, F_matrix.shape[0]):
 	for j in range(1,F_matrix.shape[1]):
@@ -107,34 +106,62 @@ i=len(sequence1)-1
 j=len(sequence2)-1
 alignment_score=0
 
-if TB_matrix[i,j]=="d":
-	seq1=seq1+sequence1[i]
-	seq2=seq2+sequence1[i]
-	alignment_score+=F_matrix[i,j]
-while len(seq1)!=len(sequence1):
-		if TB_matrix[i,j]=="d":
-			i-=1
-			j-=1
-			seq1=seq1+sequence1[i]
-			seq2=seq2+sequence1[i]
-			alignment_score+=F_matrix[i,j]
+def populate_matrices(sequence1, sequence2, scoring_matrix, gap_penalty, F_matrix, TB_matrix):
+    m, n = len(sequence1), len(sequence2)
 
-		elif TB_matrix[i,j]=='h':
-			gap_count+=1
-			j-=1
-			seq1=seq1+'-'
-			seq2=seq2+sequence2[j]
-			alignment_score+=F_matrix[i,j]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            match = F[i - 1][j - 1] + scoring_matrix.loc[sequence1[i - 1], sequence2[j - 1]]
+            gap_seq1 = F[i - 1][j] + gap_penalty
+            gap_seq2 = F[i][j - 1] + gap_penalty
 
-		elif TB_matrix[i,j]=='v':
-			gap_count+=1
-			i-=1
-			seq1+=sequence1[i]
-			seq2+='-'
-			alignment_score+=F_matrix[i,j]
+            max_score = max(match, gap_seq1, gap_seq2)
 
-print('sequence1: ',seq1)
-print('sequence2: ',seq2)
+            F_matrix[i][j] = max_score
+
+            if max_score == match:
+                TB_matrix[i][j] = 'd'  # Diagonal (match)
+            elif max_score == gap_seq1:
+                TB_matrix[i][j] = 'u'  # Up (gap in sequence 1)
+            else:
+                TB_matrix[i][j] = 'l'  # Left (gap in sequence 2)
+
+    return F_matrix, TB_matrix
+
+# while len(seq1)!=len(sequence1):
+# 		if TB_matrix[i,j]=="d":
+# 			i-=1
+# 			j-=1
+# 			seq1=seq1+sequence1[i]
+# 			seq2=seq2+sequence1[i]
+# 			alignment_score+=F_matrix[i,j]
+
+# 		elif TB_matrix[i,j]=='h':
+# 			gap_count+=1
+# 			j-=1
+# 			seq1=seq1+'-'
+# 			seq2=seq2+sequence2[j]
+# 			alignment_score+=F_matrix[i,j]
+
+# 		elif TB_matrix[i,j]=='v':
+# 			gap_count+=1
+# 			i-=1
+# 			seq1+=sequence1[i]
+# 			seq2+='-'
+# 			alignment_score+=F_matrix[i,j]
+
+F_matrix, TB_matrix = initialize_matrices(len(sequence1), len(sequence2))
+F_matrix, TB_matrix = populate_matrices(sequence1, sequence2, scoring_matrix, gap_penalty, F_matrix, TB_matrix)
+
+aligned_seq1, aligned_seq2 = traceback_alignment(TB_matrix, sequence1, sequence2)
+
+    # Count gaps and calculate alignment score
+gaps_seq1 = aligned_seq1.count('-')
+gaps_seq2 = aligned_seq2.count('-')
+alignment_score = F_matrix[len(sequence1)][len(sequence2)]
+
+print('sequence1: ',alignment_seq1)
+print('sequence2: ',alignment_seq2)
 print('gap_count: ',gap_count)
 print('alignment_score: ',alignment_score)
 
