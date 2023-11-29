ab89b788cd89bb8f01d3484b103c7a75d0205c97
Tian <ttan14@jh.edu>
2023-11-28 21:23:16 -0500

    week5 command line + vcf

--- week5/week5.py
+++ week5/week5.py
@@ -22,7 +22,13 @@
 
 # done
 
-for sample in 09 11 23 24 27 31 35 39 62 63
-do 
-	freebayes -f sacCer3.fa -p 2 --genotype-qualities A01_${sample}.srt.bam > A01_${sample}.vcf
-done
+# freebayes -f sacCer3.fa --genotype-qualities -p 1 -b A01_09.srt.bam -b A01_11.srt.bam -b A01_23.srt.bam -b A01_24.srt.bam -b A01_27.srt.bam -b A01_31.srt.bam -b A01_35.srt.bam -b A01_39.srt.bam -b A01_62.srt.bam -b A01_63.srt.bam -v A01_sample.vcf 
+
+# vcffilter -f "QUAL > 0.99" A01_sample.vcf > A01_sample_filtered.vcf
+
+# vcfallelicprimitives -k -g A01_sample_filtered.vcf > A01_sample_decomposed.vcf
+
+#snpEff download R64-1-1.86 
+snpEff ann R64-1-1.86 A01_sample_decomposed.vcf > annotated.vcf
+
+
